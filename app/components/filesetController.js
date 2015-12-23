381f29a170e0d4440dd8f3989f1a8a3901d89ebf
Álvaro Fernández González <varomorf@gmail.com>
2015-12-23 17:07:49 +0100

    dev1: implement next image button.

--- app/components/filesetController.js
+++ app/components/filesetController.js
@@ -12,6 +12,7 @@
         $scope.dirPath = '';
         $scope.currentImg = '';
         $scope.openDir = openDir;
+        $scope.nextImage = nextImage;
 
         // ATTRIBUTES
 
@@ -24,6 +25,10 @@
 
         // PUBLIC METHODS
 
+        /**
+         * <p>Shows a dialog for opening the directory to be processed.</p>
+         * <p>After a directory is selected, it will be analyzed and all images will be loaded to be organized.</p>
+         */
         function openDir() {
             dialog.showOpenDialog({defaultPath: 'C:\\', properties: ['openDirectory']}, function (fileNames) {
                 //TODO assert only one file name is present
@@ -34,10 +39,32 @@
             });
         }
 
+        /**
+         * <p>Shows the next picture of the fileset (as long as it holds at least one image).</p>
+         * <p>If the next image was selected, the next image will be the first one.</p>
+         * <p>TODO when the last image is shown and the next one is requested, show alert to user.</p>
+         */
+        function nextImage() {
+            currentFile++;
+            if (currentFile == fileSet.length) {
+                currentFile = 0;
+            }
+
+            $scope.currentImg = fileSet[currentFile];
+        }
+
         // INTERNAL METHODS
 
+        /**
+         * <p>Callback from fs.readdir to  load all images that are under $scope.dirPath and select the first one
+         * as current image.</p>
+         * @param err Errors.
+         * @param files Array with file name's from directory.
+         */
         function loadFiles(err, files) {
             //TODO check errors
+            //TODO check no files in directory
+            //TODO check no images in directory
             fileSet = [];
             currentFile = 0;
 
