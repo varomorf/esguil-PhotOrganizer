4b8f7719a44dfb4643aaa081ceee11d5c162f670
Álvaro Fernández González <varomorf@gmail.com>
2016-09-26 15:46:31 +0200

    Remove js and js.mp files compiled by Webstorm.

--- app/bootstrap.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const app_1 = require('./app');
-platform_browser_dynamic_1.bootstrap(app_1.AppComponent);
-//# sourceMappingURL=bootstrap.js.map
